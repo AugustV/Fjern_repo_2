787e9c28953be8567435f287814f51b0e3fff4eb
nikl2513 <nikl2513@outlook.dk>
2022-09-26 15:07:44 +0200

    ingen semikolon

--- src/ASCII_Tabel.java
+++ src/ASCII_Tabel.java
@@ -30,7 +30,7 @@ public class ASCII_Tabel {
                 }
                 System.out.println("stop med at drikke så meget cacomilk");
                 System.out.println("Kage");
-                System.out.println("det smager rigtig flot");
+                System.out.println("det smager rigtig flot")
 
 
             }
