e4e69d027bfc95145147fb8dd3c9bace7c228910
nikl2513 <nikl2513@outlook.dk>
2022-09-26 15:15:07 +0200

    fejl
    jnbjb

--- src/ASCII_Tabel.java
+++ src/ASCII_Tabel.java
@@ -31,6 +31,8 @@ public class ASCII_Tabel {
                 System.out.println("stop med at drikke så meget cacomilk");
                 System.out.println("Kage");
                 System.out.println("det smager rigtig flot");
+                soutln
+
                 System.out.println("det smager rigtig flot");
                 sout den her commando er invalid
                         nu gør jeg mere lort
